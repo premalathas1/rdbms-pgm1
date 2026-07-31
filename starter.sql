389ec00072bfe46e0f0bff8e9f1a18769583f811
premalathas1 <premalathas@rvsgroup.com>
2026-07-31 09:37:49 +0530

    Create starter.sql

---
+++ starter.sql
@@ -0,0 +1,18 @@
+-- CollegeDB Database Creation Assignment
+-- Student Name:
+-- Register Number:
+
+-- Write SQL queries below
+
+-- Create database named CollegeDB
+
+
+-- Select CollegeDB database
+
+
+-- Create Department table with:
+-- DepartmentID Number(5) Primary Key
+-- DepartmentName Varchar(20)
+-- HOD Varchar(20)
+
+
